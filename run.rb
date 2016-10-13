7a8040b011e3706d5d0f6c3478b0dd2f029bea4e
Cate Devey <cdevey@nd.edu>
2016-10-13 09:03:31 +0800

    without NY as default

--- run.rb
+++ run.rb
@@ -8,6 +8,47 @@ get '/' do
 end
 
 get '/states' do
+  #create an array called states
+  @states=[]
+
+  #create a hash called states
+  @state={
+    :id => 'NY',
+    :name => 'New York'
+  }
+
+  @states <<@state
+
+  @state={
+    :id => 'AL',
+    :name => 'Alabama'
+  }
+
+  @states <<@state
+
+  @state={
+    :id => 'ME',
+    :name => 'Maine'
+  }
+
+  @states <<@state
+
+  @state={
+    :id => 'MA',
+    :name => 'Massachusetts'
+  }
+
+  @states <<@state
+
+  @state={
+    :id => 'NJ',
+    :name => 'New Jersey'
+  }
+
+  #add hash to the array
+  @states <<@state
+
+  @states.sort_by!{|s| s[:name]}
   # code!
   erb :states, layout: :main
 end
